d88eea04fcd354651bcea08324a301ddd20bcd08
Onwebbe <onwebbe@hotmail.com>
2020-04-27 00:04:52 +0800

    add raindrop script

--- rasiberryPiGPIOAPIMain/saveAllData.sh
+++ rasiberryPiGPIOAPIMain/saveAllData.sh
@@ -2,4 +2,4 @@
 /myProjects/python/rasiberryPiGPIOAPI/rasiberryPiGPIOAPIMain/getDHT22Data.sh
 /myProjects/python/rasiberryPiGPIOAPI/rasiberryPiGPIOAPIMain/getBMP180Data.sh
 /myProjects/python/rasiberryPiGPIOAPI/rasiberryPiGPIOAPIMain/getGY30Data.sh
-
+/myProjects/python/rasiberryPiGPIOAPI/rasiberryPiGPIOAPIMain/getRainDropData.sh
